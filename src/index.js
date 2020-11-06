bd79f336fabfe7d6a381a70aa822b98b49a64e89
jt5493 <jt5493@hotmail.com>
2020-11-06 20:55:35 +0900

    added imports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import { thunk } from 'redux-thunk'
+
+// const store
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
