bdff3a7b3d69c925b08fdb31ef0af16476742380
jt5493 <jt5493@hotmail.com>
2020-11-07 17:02:57 +0900

    created binder reducer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
-import { thunk } from 'redux-thunk'
+import ReduxThunk from 'redux-thunk';
+import binderReducer from './reducers/binderReducer'
 
-const enhancer = compose()
+
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(binderReducer, composeEnhancers(applyMiddleware(ReduxThunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/binderReducer.js
@@ -0,0 +1,6 @@
+export default function binderReducer(state = {binders: []}, action) {
+
+    return state
+
+
+}
