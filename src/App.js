57d8b2eedd43f931ea7c245321087333a051f6a7
jt5493 <jt5493@hotmail.com>
2020-11-07 13:41:55 +0900

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/binders')
+    .then(r => r.json())
+    .then(res => console.log(res))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          hello
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
